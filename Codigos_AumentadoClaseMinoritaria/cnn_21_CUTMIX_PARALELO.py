fa2c58cbab724588357f63ee6cbedabc742eb6f9
DBR5823 <berdullasdaniel@gmail.com>
2026-03-07 18:01:46 +0100

    Arreglado pixeles output

--- Codigos_AumentadoClaseMinoritaria/cnn_21_CUTMIX_PARALELO.py
+++ Codigos_AumentadoClaseMinoritaria/cnn_21_CUTMIX_PARALELO.py
@@ -919,6 +919,11 @@ def main(exp, alpha, data_bundle, TEST, EPOCHS, BATCH, probabilidad, usar_sample
       #Cada vez que se han clasificado 2000 patches se imprime por pantalla el progreso del testeo
       #if(total%2000==0): #print('  Testeando: %6d/%d'%(total,len(dataset_test)))
 
+  #Eliminamos los centros usados en el entrenamiento y validación de la red, de esta manera en el output todos valdrán 0
+  for i in train: output[i]=0
+  for i in val: output[i]=0
+  
+  
   #Tras lo anterior tenemos el mapa con únicamente la clasificación de los píxeles que son centros de segmento, por tanto se debe propagar la clase del centro del segmento a los píxeles del segmento completo
   #print('* Generando mapa de clasificación (only ground-truth) (solo segmentos usados en el testeo)')
   #Recorremos todos los píxeles del output
@@ -927,9 +932,7 @@ def main(exp, alpha, data_bundle, TEST, EPOCHS, BATCH, probabilidad, usar_sample
   #Miramos que clase fue asignada al píxel central (mediante el patch) y le asignamos esa clase al píxel actual
   for i in range(H*V): output[i]=output[center[seg[i]]]
 
-  #Eliminamos los centros usados en el entrenamiento y validación de la red, de esta manera en el output todos valdrán 0
-  for i in train: output[i]=0
-  for i in val: output[i]=0
+  
   
   # 9. Calculamos las precisiones por segmentos (excluyendo los usados en el entrenamiento y validación)
   #Contadores para clasificaciones correctas de segmentos y el total de segmentos
